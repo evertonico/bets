12045c066e2deba444328f7cd61c6bd3bd5e3c01
evertonico <evertonce@gmail.com>
2021-04-20 20:50:36 -0300

    Configurada conexão com o Banco PostgreSQL

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -15,8 +15,11 @@
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?= base_url('public/dist/css/adminlte.min.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('public/dist/css/skin-green.min.css'); ?>">
     <!-- summernote -->
     <link rel="stylesheet" href="<?= base_url('public/plugins/summernote/summernote-bs4.min.css'); ?>">
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <!--
 `body` tag options:
