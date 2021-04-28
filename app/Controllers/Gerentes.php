2ae9b939869136ce8c7d35ac258a3c3bcd6a9070
evertonico <evertonce@gmail.com>
2021-04-28 16:36:35 -0300

    Método para trazer lista de permissões disponíveis pro gerente

--- app/Controllers/Gerentes.php
+++ app/Controllers/Gerentes.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\RegulamentoModel;
+use Config\App;
 
 helper('App\Helpers\template_helper');
 
@@ -19,17 +20,21 @@ class Gerentes extends BaseController
      */
     public function index()
     {
-
         $data['title'] = TITULO_GERENTE;
         $model = new RegulamentoModel();
         $data['result'] = $model->first();
         return view('gerentes/listar', ['data' => $data]);
-
     }
 
+    /**
+     * @return string
+     */
     public function cadastro()
     {
         $data['title'] = TITULO_GERENTE;
+        $modelPermissao = new \App\Models\PermissaoModel();
+        # buscando permissões disponíveis para o Tipo de Usuário Gerente ( id = 3)
+        $data['listPermissoes'] = $modelPermissao->find(3);
         return view('gerentes/cadastro', ['data' => $data]);
     }
 
