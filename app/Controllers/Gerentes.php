6e335fbd507ec63a6960dcb200980820369b3124
evertonico <evertonce@gmail.com>
2021-04-28 17:48:36 -0300

    Início das validações

--- app/Controllers/Gerentes.php
+++ app/Controllers/Gerentes.php
@@ -33,7 +33,7 @@ class Gerentes extends BaseController
     {
         $data['title'] = TITULO_GERENTE;
         $modelPermissao = new \App\Models\PermissaoModel();
-        // buscando permissões disponíveis para o Tipo de Usuário Gerente ( id = 3)
+        // buscando permissões disponíveis para o Tipo de Usuário Gerente (id = 3)
         $data['listPermissoes'] = $modelPermissao->findByTipoUsuario(3);
         return view('gerentes/cadastro', ['data' => $data]);
     }
@@ -68,7 +68,7 @@ class Gerentes extends BaseController
             // Verificando se passou na validação
             if (!$res) {
                 $modelPermissao = new \App\Models\PermissaoModel();
-                // buscando permissões disponíveis para o Tipo de Usuário Gerente ( id = 3)
+                // buscando permissões disponíveis para o Tipo de Usuário Gerente (id = 3)
                 $data['listPermissoes'] = $modelPermissao->findByTipoUsuario(3);
                 // Chamando a view e enviando os erros de validação
                 echo view('gerentes/cadastro', [
