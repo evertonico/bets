1f3fe7fad4d155aa49ad627afb9ded00f9996f68
evertonico <evertonce@gmail.com>
2021-04-29 20:49:17 -0300

    Correção nos comentários do código

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 /**
  * Class UsuarioModel
  *
- * Model Permissões
+ * Model Usuário
  *
  * @package App\Entities
  */
@@ -44,7 +44,7 @@ class UsuarioModel extends Model
         'ds_email' => 'required|valid_email|is_unique[tb_usuario.ds_email]',
         'ds_senha' => 'required|min_length[6]',
         'cd_tipo_usuario' => 'required',
-        'fl_bloqueado' => 'required' 
+        'fl_bloqueado' => 'required'
     ];
 
     // Mensagens de validação
@@ -77,6 +77,5 @@ class UsuarioModel extends Model
     ];
 
     protected $skipValidation = false;
-
 
 }
